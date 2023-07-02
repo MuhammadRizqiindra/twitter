f0537ee699bd59a58bc7f2aa3d56c8acd68549b9
MuhammadRizqiindra <hudariski578@gmail.com>
2023-07-02 13:05:14 +0700

    add Appwirite API

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,10 +1,10 @@
 class AppwriteConstants {
-  static const String databaseId = '64a0f648e3c561869458';
+  static const String databaseId = '64a10ff36b0fc470c973';
   static const String projectId = '6423ba26ef6c810b3753';
   static const String endPoint = 'https://baas.pasarjepara.com/v1';
 
   static const String usersCollection = '63cb8ae73960973b0635';
-  static const String tweetsCollection = '63cbd6781a8ce89dcb95';
+  static const String tweetsCollection = '64a1100b2dbec625422d';
   static const String notificationsCollection = '63cd5ff88b08e40a11bc';
 
   static const String imagesBucket = '63cbdab48cdbccb6b34e';
